c3868e80fea0bf3392764dd9cea7a1614ac2cf45
Enrique García Cota <kikito@gmail.com>
2011-04-20 01:06:23 +0200

    updated comment on memoize

--- memoize.lua
+++ memoize.lua
@@ -1,10 +1,35 @@
 -----------------------------------------------------------------------------------------------------------------------
--- memoize.lua - v0.1 (2011-04)
+-- memoize.lua - v1.0 (2011-04)
 -- Enrique García Cota - enrique.garcia.cota [AT] gmail [DOT] com
 -- memoize lua functions easily
 -- Based on http://stackoverflow.com/questions/129877/how-do-i-write-a-generic-memoize-function
 -----------------------------------------------------------------------------------------------------------------------
 
+--[[ usage:
+
+local memoize = require 'memoize' -- or memoize.lua, depending on your env.
+
+function slowFunc(param1, param2, param3)
+  -- do something expensive, like calculating a value or reading a file
+end
+
+local memoizedSlowFunc = memoize(slowFunc)
+
+-- First execution takes some time, but the system caches the result
+memoizedSlowFunc('a','b') -- first time is slow
+
+-- Second execution is fast, since the system uses the cache
+memoizedSlowFunc('a','b') -- second time is fast
+memoizedSlowFunc('a','b') -- from now on, it is fast
+
+-- This happens with every new combination of params
+memoizedSlowFunc('c','d') -- slow
+memoizedSlowFunc('e','f') -- slow
+memoizedSlowFunc('c','d') -- fast
+]]
+
+
+-- private stuff
 
 local globalCache = {}
 
@@ -32,6 +57,8 @@ local function insertInCache(cache, args, result)
   node.value = result
 end
 
+
+-- public function
 
 local function memoize(f)
   globalCache[f] = {}

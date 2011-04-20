321f581f99337584ae6b3f99177d4f4133724eda
Enrique García <kikito@gmail.com>
2011-04-20 13:46:01 +0200

    added README

--- memoize.lua
+++ memoize.lua
@@ -2,30 +2,31 @@
 -- memoize.lua - v0.9 (2011-04)
 -- Enrique García Cota - enrique.garcia.cota [AT] gmail [DOT] com
 -- memoize lua functions easily
--- Based on http://stackoverflow.com/questions/129877/how-do-i-write-a-generic-memoize-function
+-- Inspired by http://stackoverflow.com/questions/129877/how-do-i-write-a-generic-memoize-function
 -----------------------------------------------------------------------------------------------------------------------
 
 --[[ usage:
 
 local memoize = require 'memoize' -- or memoize.lua, depending on your env.
 
-function slowFunc(param1, param2, param3)
+function slowFunc(param1, param2)
   -- do something expensive, like calculating a value or reading a file
+  return somethingSlow
 end
 
 local memoizedSlowFunc = memoize(slowFunc)
 
 -- First execution takes some time, but the system caches the result
-memoizedSlowFunc('a','b') -- first time is slow
+x = memoizedSlowFunc('a','b') -- first time is slow
 
 -- Second execution is fast, since the system uses the cache
-memoizedSlowFunc('a','b') -- second time is fast
-memoizedSlowFunc('a','b') -- from now on, it is fast
+x = memoizedSlowFunc('a','b') -- second time is fast
+x = memoizedSlowFunc('a','b') -- from now on, it is fast
 
 -- This happens with every new combination of params
-memoizedSlowFunc('c','d') -- slow
-memoizedSlowFunc('e','f') -- slow
-memoizedSlowFunc('c','d') -- fast
+y = memoizedSlowFunc('c','d') -- slow
+z = memoizedSlowFunc('e','f') -- slow
+y = memoizedSlowFunc('c','d') -- fast
 ]]
 
 
@@ -34,18 +35,16 @@ memoizedSlowFunc('c','d') -- fast
 local globalCache = {}
 
 local function getFromCache(cache, args)
-  local arg, i
   local node = cache
   for i=1, #args do
-    arg = args[i]
-    node.children = node.children or {}
-    node = node.children[arg]
-    if node == nil then return nil end
+    if not node.children then return {} end
+    node = node.children[args[i]]
+    if not node then return {} end
   end
-  return node.value
+  return node.results
 end
 
-local function insertInCache(cache, args, result)
+local function insertInCache(cache, args, results)
   local arg, i
   local node = cache
   for i=1, #args do
@@ -54,22 +53,23 @@ local function insertInCache(cache, args, result)
     node.children[arg] = node.children[arg] or {}
     node = node.children[arg]
   end
-  node.value = result
+  node.results = results
 end
 
 
 -- public function
 
 local function memoize(f)
-  globalCache[f] = {}
+  globalCache[f] = { results = {} }
   return function (...)
-    local result = getFromCache( globalCache[f], {...} )
+    local results = getFromCache( globalCache[f], {...} )
       
-    if result == nil then
-      result = f(...)
-      insertInCache(globalCache[f], {...}, result)
-      return result
+    if #results == 0 then
+      results = { f(...) }
+      insertInCache(globalCache[f], {...}, results)
     end
+    
+    return unpack(results)
   end
 end
 
